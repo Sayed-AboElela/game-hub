eca57f5505a6353c2acaa57ce710a35186a529ea
Sayed-AboElela <sayed.aboelela96@gmail.com>
2025-02-20 17:30:09 +0200

    Navbar strucure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import './App.css'
-import {Grid, GridItem} from "@chakra-ui/react";
+import "./App.css";
+import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "@/components/NavBar.tsx";
 
 function App() {
-    return (
-        <Grid templateAreas={{
-            base: `"nav" "main"`,
-            lg: `"nav nav" "aside main"`,
-        }}>
-            <GridItem area={'nav'}>Nav</GridItem>
-            <GridItem hideBelow="lg" area={'aside'}>aside</GridItem>
-            <GridItem area={'main'}>main</GridItem>
-        </Grid>
-    )
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+
+      <GridItem hideBelow="lg" area={"aside"}>
+        aside
+      </GridItem>
+      <GridItem area={"main"}>main</GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
