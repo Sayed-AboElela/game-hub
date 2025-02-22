3b1e8eaf451c1447ede095a91c7445ede325534d
Sayed-AboElela <sayed.aboelela96@gmail.com>
2025-02-22 18:40:11 +0200

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,8 @@
-import { Genre } from "@/services/genres-service";
-import useData from "./useData";
+import genres from "@/data/genres.ts";
+//import useData from "@/hooks/useData.ts";
+//import {Genre} from "@/services/genres-service.ts";
 
-const useGenres = () => useData<Genre>('/genres');
+//const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data: genres, isLoading: false, error: null})
 
 export default useGenres;
